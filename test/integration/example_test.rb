00aef7c6a8041795e34c948a06439d30fb9176fd
Sean Handley <sean.handley@gmail.com>
2015-08-10 12:59:27 +0100

    Integration tests for domains.

--- test/integration/example_test.rb
+++ test/integration/example_test.rb
@@ -2,15 +2,22 @@ require_relative '../test_helper'
 require_relative '../integration_test_helper'
 
 module Deliverhq
-  class ExampleTest < Minitest::Test
+  class DomainsTest < Minitest::Test
     
     def setup
-      
+      @domain_name = "foobar.com"
     end
     
-    def test_something
-      
+    def test_create_list_and_delete_domains
+      VCR.use_cassette('crd_domains') do
+        domain_count = Domain.all.count
+        domain = Domain.create(name: @domain_name)
+        assert_equal domain.name, @domain_name
+        assert_equal domain_count+1, Domain.all.count
+        assert domain.destroy
+        assert_equal domain_count, Domain.all.count
+      end
     end
-    
+  
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,6 +10,9 @@ require_relative '../lib/deliverhq'
 
 TEST_ENV = true
 
+ENV['DELIVERHQ_ID'] = 'foo'
+ENV['DELIVERHQ_KEY'] = 'bar'
+
 require 'rubygems'
 gem "minitest"
 require 'minitest/autorun'
