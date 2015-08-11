dbca9b1d937e1a22059c820e6f67d424631fb762
Sean Handley <sean.handley@gmail.com>
2015-08-11 10:29:11 +0100

    Reorder loading.

--- lib/deliverhq.rb
+++ lib/deliverhq.rb
@@ -3,19 +3,25 @@ require 'json'
 require 'date'
 
 module Deliverhq
-  require_relative 'deliverhq/models/domain'
-  require_relative 'deliverhq/models/mailbox'
-  require_relative 'deliverhq/models/message'
+
   require_relative 'deliverhq/request/base'
+  require_relative 'deliverhq/response'
+
   require_relative 'deliverhq/request/domain/create'
   require_relative 'deliverhq/request/domain/delete'
   require_relative 'deliverhq/request/domain/list'
+
   require_relative 'deliverhq/request/mailbox/create'
   require_relative 'deliverhq/request/mailbox/delete'
   require_relative 'deliverhq/request/mailbox/list'
+
   require_relative 'deliverhq/request/message/list'
   require_relative 'deliverhq/request/message/show'
-  require_relative 'deliverhq/response'
+  
+  require_relative 'deliverhq/models/domain'
+  require_relative 'deliverhq/models/mailbox'
+  require_relative 'deliverhq/models/message'
+  
 
   class RequestError < StandardError; end
 
