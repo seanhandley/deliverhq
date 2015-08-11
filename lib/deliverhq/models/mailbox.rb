e62484d5544ee292ce261257d42ae4d590b80d3e
Sean Handley <sean.handley@gmail.com>
2015-08-11 09:58:20 +0100

    Add missing mailbox parameters.

--- lib/deliverhq/models/mailbox.rb
+++ lib/deliverhq/models/mailbox.rb
@@ -3,7 +3,10 @@ module Deliverhq
     
     attr_reader :id, :name, :check_spam, :destination_address,
                 :destination_type, :domain_id, :encoding, :format,
-                :reply_stripping, :partial_address
+                :reply_stripping, :partial_address,
+                :account_id, :server_id, :domain,
+                :address, :created_at, :forwarding_token,
+                :verification_token,:verified_at
 
     def initialize(params)
       @id = params['id']
@@ -16,6 +19,14 @@ module Deliverhq
       @format = params["format"]
       @reply_stripping = params["reply_stripping"]
       @partial_address = params["partial_address"]
+      @account_id = params["account_id"]
+      @server_id = params["server_id"]
+      @domain = params["domain"]
+      @address = params["address"]
+      @created_at = params["created_at"]
+      @forwarding_token = params["forwarding_token"]
+      @verification_token = params["verification_token"]
+      @verified_at = params["verified_at"]
     end
 
     def self.create(params)
